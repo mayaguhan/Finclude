f0fd3794328c454276f5ee50e81d17ae9ecc6ab6
mayaguhan <i.am.maya.guhan@gmail.com>
2021-08-27 17:37:43 +0800

    Initial Commit
    Vuetify Added. Router added.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <div id="app">
+      <Header />
+    </div>
+    <div>
+      <router-view></router-view>
+    </div>
+  </v-app>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue"
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+} */
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <v-app-bar
+      color="cyan accent-4"
+      dense
+      dark
+    >
+      <v-toolbar-title>
+        <router-link to="/" id="textRouterLink" style="text-decoration: none; color: white">
+          <img src="../assets/b8520d9c105e469b92efd98de1d7772d.png" style="height: 80px; margin-top: 10px;">
+        </router-link> 
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <router-link to="/about" id="textRouterLink" style="margin-right: 7px">About</router-link>
+
+      <span style="margin-right: 7px">Calculator</span>
+
+      <span>Savings</span>
+
+
+      <!-- <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu> -->
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+
+    data () {
+      return {
+        //
+      }
+    },
+    methods: {
+        //
+    },
+    created() {
+      //
+    }
+  }
+</script>
+
+
+<style scoped>
+
+#textRouterLink {
+  text-decoration: none;
+  color: white;
+}
+  
+</style>
