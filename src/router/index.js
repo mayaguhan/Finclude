12ebe2e65c942e560acc783c7dd04de1f8cde37c
mayaguhan <i.am.maya.guhan@gmail.com>
2021-08-28 16:00:19 +0800

    Minor Updates

--- src/router/index.js
+++ src/router/index.js
@@ -32,6 +32,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Savings.vue')
+  },
+  {
+    path: '/probonoform',
+    name: 'ProBonoForm',
+    component: () => import('../views/Forms/ProBono.vue')
+  },
+  {
+    path: '/userform',
+    name: 'UserForm',
+    component: () => import('../views/Forms/User.vue')
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,34 @@
 <template>
-    <div style="text-align: center" class="mt-3">
-        <h1>If we make it to Top 40...</h1>
-    </div>
+            <v-container>
+                <v-row>
+                    <v-col>
+                        <img src="../assets/amy-hirschi-JaoVGh5aJ3E-unsplash.jpg" style="height: 500px; border-radius: 10px">
+                    </v-col>
+                    <v-col>
+                        <v-row id="user">
+                            <v-col class="mt-4">
+                                <img src="../assets/businessman.png" style="height: 175px">
+                                <router-link to="/probonoform" style="text-decoration: none">
+                                    <v-btn text>
+                                        I want to be a pro-bono advisor!
+                                    </v-btn>
+                                </router-link>
+                            </v-col>
+                        </v-row>
+                        <v-row id="user">
+                            <v-col class="mt-2">
+                                <img src="../assets/thinking.jpg" style="height: 175px">
+
+                                <router-link to="/userform" style="text-decoration: none">
+                                    <v-btn text>
+                                        I want financial advice!
+                                    </v-btn>
+                                </router-link>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                </v-row>
+            </v-container>
 </template>
 
 <script>
@@ -23,5 +50,9 @@ export default {
 </script>
 
 <style>
-
+#user {
+    text-align: center;
+    align-content: center;
+    margin: auto;
+}
 </style>
